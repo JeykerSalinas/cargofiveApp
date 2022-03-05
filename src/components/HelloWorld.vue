d876ee9ff396db47493c3f510b289fff0184b133
JeykerSalinas <66428501+JeykerSalinas@users.noreply.github.com>
2022-03-05 18:56:38 -0300

    conecting api,data rendered

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,11 +1,50 @@
 <template>
-  <div class="hello"></div>
+  <div class="hello">
+    <table class="table table-dark table-striped">
+      <thead>
+        <tr>
+          <th scope="col">Id</th>
+          <th scope="col">Name</th>
+          <th scope="col">Country</th>
+          <th scope="col">Continent</th>
+          <th scope="col">Coordinates</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="d in ports" v-bind:key="d.id">
+          <td>{{ d.id }}</td>
+          <td>{{ d.name }}</td>
+          <td>{{ d.country }}</td>
+          <td>{{ d.continent }}</td>
+          <td>{{ d.coordinates }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
 export default {
   name: "HelloWorld",
-  async mounted() {},
+  data() {
+    return {
+      pageNumber: 1,
+      ports: [],
+    };
+  },
+  methods: {
+    async getData() {
+      const res = await fetch(
+        `http://apitest.cargofive.com/api/ports?page=${this.pageNumber}`
+      );
+      const data = await res.json();
+      this.ports = data.data;
+      console.log(this.ports);
+    },
+  },
+  mounted() {
+    this.getData();
+  },
 };
 </script>
 
